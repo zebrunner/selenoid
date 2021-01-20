f466c5de39102ea559ea70c5ff41d98e6d39db7d
vdelendik <vdelendik@zebrunner.com>
2021-01-20 14:40:29 +0000

    #16: organize independent setup process for selenoid

--- zebrunner.sh
+++ zebrunner.sh
@@ -2,17 +2,25 @@
 
   setup() {
 
+    if [[ $ZBR_INSTALLER -eq 0 ]]; then
+      # load default interactive installer settings
+      source backup/settings.env.original
+
+      # setup executed outside of zebrunner community edition. need ask about S3 compatible storage credentials
+      set_aws_storage_settings
+    fi
+
     cp .env.original .env
-    if [[ $ZBR_MINIO_ENABLED -eq 0 ]]; then
+    if [[ ! $ZBR_MINIO_ENABLED -eq 1 ]]; then
       # use case with AWS S3
-      sed -i "s#S3_REGION=us-east-1#S3_REGION=${ZBR_STORAGE_REGION}#g" .env
-      sed -i "s#S3_ENDPOINT=http://minio:9000#S3_ENDPOINT=${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}#g" .env
-      sed -i "s#S3_BUCKET=zebrunner#S3_BUCKET=${ZBR_STORAGE_BUCKET}#g" .env
-      sed -i "s#S3_ACCESS_KEY_ID=zebrunner#S3_ACCESS_KEY_ID=${ZBR_STORAGE_ACCESS_KEY}#g" .env
-      sed -i "s#S3_SECRET=J33dNyeTDj#S3_SECRET=${ZBR_STORAGE_SECRET_KEY}#g" .env
+      replace .env "S3_REGION=us-east-1" "S3_REGION=${ZBR_STORAGE_REGION}"
+      replace .env "S3_ENDPOINT=http://minio:9000" "S3_ENDPOINT=${ZBR_STORAGE_ENDPOINT_PROTOCOL}://${ZBR_STORAGE_ENDPOINT_HOST}"
+      replace .env "S3_BUCKET=zebrunner" "S3_BUCKET=${ZBR_STORAGE_BUCKET}"
+      replace .env "S3_ACCESS_KEY_ID=zebrunner" "S3_ACCESS_KEY_ID=${ZBR_STORAGE_ACCESS_KEY}"
+      replace .env "S3_SECRET=J33dNyeTDj" "S3_SECRET=${ZBR_STORAGE_SECRET_KEY}"
 
       if [[ ! -z $ZBR_STORAGE_TENANT ]]; then
-        sed -i "s#/artifacts#${ZBR_STORAGE_TENANT}/artifacts#g" .env
+        replace .env "/artifacts" "${ZBR_STORAGE_TENANT}/artifacts"
       fi
     fi
 
@@ -120,6 +128,91 @@
     echo "selenoid: ${TAG_SELENOID}"
   }
 
+  # https://github.com/zebrunner/zebrunner/issues/384 investigate possibility to make sub-components configurable independently
+  # https://github.com/zebrunner/selenoid/issues/16 investigate possibility to make selenoid auto-configurable
+  # IMPORTANT! copy of this method exists in root zebrunner.sh and maybe will be added to reporting/zebrunner.sh
+  set_aws_storage_settings() {
+    ## AWS S3 storage
+    local is_confirmed=0
+    #TODO: provide a link to documentation howto create valid S3 bucket
+    echo
+    echo "AWS S3 storage"
+    while [[ $is_confirmed -eq 0 ]]; do
+      read -p "Region [$ZBR_STORAGE_REGION]: " local_region
+      if [[ ! -z $local_region ]]; then
+        ZBR_STORAGE_REGION=$local_region
+      fi
+
+      ZBR_STORAGE_ENDPOINT_PROTOCOL="https"
+      ZBR_STORAGE_ENDPOINT_HOST="s3.${ZBR_STORAGE_REGION}.amazonaws.com:443"
+
+      read -p "Bucket [$ZBR_STORAGE_BUCKET]: " local_bucket
+      if [[ ! -z $local_bucket ]]; then
+        ZBR_STORAGE_BUCKET=$local_bucket
+      fi
+
+      read -p "Access key [$ZBR_STORAGE_ACCESS_KEY]: " local_access_key
+      if [[ ! -z $local_access_key ]]; then
+        ZBR_STORAGE_ACCESS_KEY=$local_access_key
+      fi
+
+      read -p "Secret key [$ZBR_STORAGE_SECRET_KEY]: " local_secret_key
+      if [[ ! -z $local_secret_key ]]; then
+        ZBR_STORAGE_SECRET_KEY=$local_secret_key
+      fi
+
+      if [[ $ZBR_REPORTING_ENABLED -eq 0 ]]; then
+        export ZBR_MINIO_ENABLED=0
+        read -p "[Optional] Tenant [$ZBR_STORAGE_TENANT]: " local_value
+        if [[ ! -z $local_value ]]; then
+          ZBR_STORAGE_TENANT=$local_value
+        fi
+      else
+        read -p "UserAgent key [$ZBR_STORAGE_AGENT_KEY]: " local_agent_key
+        if [[ ! -z $local_agent_key ]]; then
+          ZBR_STORAGE_AGENT_KEY=$local_agent_key
+        fi
+      fi
+
+      echo "Region: $ZBR_STORAGE_REGION"
+      echo "Endpoint: $ZBR_STORAGE_ENDPOINT_PROTOCOL://$ZBR_STORAGE_ENDPOINT_HOST"
+      echo "Bucket: $ZBR_STORAGE_BUCKET"
+      echo "Access key: $ZBR_STORAGE_ACCESS_KEY"
+      echo "Secret key: $ZBR_STORAGE_SECRET_KEY"
+      echo "Agent key: $ZBR_STORAGE_AGENT_KEY"
+      echo "Tenant: $ZBR_STORAGE_TENANT"
+      confirm "" "Continue?" "y"
+      is_confirmed=$?
+    done
+
+    export ZBR_STORAGE_REGION=$ZBR_STORAGE_REGION
+    export ZBR_STORAGE_ENDPOINT_PROTOCOL=$ZBR_STORAGE_ENDPOINT_PROTOCOL
+    export ZBR_STORAGE_ENDPOINT_HOST=$ZBR_STORAGE_ENDPOINT_HOST
+    export ZBR_STORAGE_BUCKET=$ZBR_STORAGE_BUCKET
+    export ZBR_STORAGE_ACCESS_KEY=$ZBR_STORAGE_ACCESS_KEY
+    export ZBR_STORAGE_SECRET_KEY=$ZBR_STORAGE_SECRET_KEY
+    export ZBR_STORAGE_AGENT_KEY=$ZBR_STORAGE_AGENT_KEY
+  }
+
+  replace() {
+    #TODO: https://github.com/zebrunner/zebrunner/issues/328 organize debug logging for setup/replace
+    file=$1
+    #echo "file: $file"
+    content=$(<$file) # read the file's content into
+    #echo "content: $content"
+
+    old=$2
+    #echo "old: $old"
+
+    new=$3
+    #echo "new: $new"
+    content=${content//"$old"/$new}
+
+    #echo "content: $content"
+
+    printf '%s' "$content" >$file    # write new content to disk
+  }
+
   echo_warning() {
     echo "
       WARNING! $1"
@@ -137,7 +230,7 @@
       Flags:
           --help | -h    Print help
       Arguments:
-          setup          Download two latest versions of chrome, firefox and opera browsers
+          setup          Download 2 latest versions of chrome, firefox and opera browsers. Configure S3 storage integration.
       	  start          Start container
       	  stop           Stop and keep container
       	  restart        Restart container
